95fb13181ea1cfc5321dec4ecc7520bed2b6b8a2
dboyce77 <dboyce77@gmail.com>
2018-10-31 13:35:17 -0500

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,23 @@
 //schema.sql
-DROP bamzon IF EXISTS
+DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
-USE bamazon
+USE bamazon;
 
 CREATE TABLE products (
-    item_id INT NOT NULL Auto_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100),
     department_name VARCHAR(50),
     price INT,
-    stock_quantity INT
-    PRIMARY KEY(item_id)
-)
+    stock_quantity INT,
+    PRIMARY KEY(id)
+);
 
-INSERT INTO products (product_name, department_name, stock_quantity)
-VALUES ("Samsung TV", "Electronics", 150, 100), ("Great Plains Tent", "Outdoors", 49, 50), ("Toro 22 inch mower", "Lawn and Garden", 325, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Samsung TV", "Electronics", 1000, 100), ("Great Plains Tent", "Outdoors", 49, 50), ("Toro 22 inch mower", "Lawn and Garden", 325, 50);
 
-INSERT INTO products (product_name, department_name, stock_quantity)
-VALUES ("HP Laptop", "Electronics", 750, 25), ("Propane Lantern", "Outdoors", 25, 10), ("Toro Gas Blower", "Lawn and Garden", 125, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bamazon player", "Electronics", 1000, 100), ("Great Plains Lantern", "Outdoors", 25, 50), ("Toro Blower", "Lawn and Garden", 50, 60);
 
-INSERT INTO products (product_name, department_name, stock_quantity)
-VALUES ("Bamzon music player", "Electronics", 50, 25), ("Sleeping Bags", "Outdoors", 25, 15), ("Hedge clippers", "Lawn and Garden", 25, 15), ("Sunlight Toaster", "Home Electronics", 15, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("HP 15 inch Laptop", "Electronics", 750, 200), ("Great Plains Sleeping Bags", "Outdoors", 25, 10), ("Hedge Trimmers", "Lawn and Garden", 25, 50), ("Stuffed Bear", "Toys", 25, 150);
